f6def1680c3aeb1a19d39a2ea604e5c5b4cca19d
Usuário <thiagocaboski28@gmail.com>
2026-01-14 19:19:29 -0300

    Primeiro commit do app Elo Digital

---
+++ supabase/functions/elo-api-server/deno.json.ts
@@ -0,0 +1,13 @@
+{
+  "imports": {
+    "npm:hono": "npm:hono@^3.12.0",
+    "npm:hono/cors": "npm:hono@^3.12.0/cors",
+    "npm:hono/logger": "npm:hono@^3.12.0/logger",
+    "npm:@supabase/supabase-js@2": "npm:@supabase/supabase-js@^2.43.2",
+    "npm:jwt-decode": "npm:jwt-decode@^4.0.0"
+  },
+  "compilerOptions": {
+    "jsx": "react-jsx",
+    "jsxImportSource": "react"
+  }
+}
